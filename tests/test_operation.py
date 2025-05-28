98e09eaf59ce7b3c803f5ae556acb9ea1058556f
PhucNg2k <22521134@gm.uit.edu.vn>
2025-05-28 22:58:47 +0700

    add unit test workflow

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -3,11 +3,11 @@ from src.math_operations import add, sub
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
-    assert(-8, 3)==-5
+    assert add(-8, 3)==-5
     
 def test_sub():
     assert sub(5,3)==2
     assert sub(1,8)==-7
     assert sub(3,3)==0
 
-    
+
